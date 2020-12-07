bc23cd45ff79af57f38d0da770173d2f30d5a07d
Mirek Simek <miroslav.simek@vscht.cz>
2020-12-07 19:37:22 +0100

    deletion test

--- tests/test_rest_delete.py
+++ tests/test_rest_delete.py
@@ -12,9 +12,14 @@ def test_rest_delete_not_authorized(api_client, es, sample_records, login, enrol
     assert resp.status_code == 403
 
 
-def test_rest_delete_authorized(api_client, es, sample_records, login, record_deleting_user):
-    login(api_client, record_deleting_user.id)
+def test_rest_delete_authorized(api_client, es, sample_records, login, record_deleting_user, record_updating_user):
+    login(api_client, record_updating_user.id)
+    resp = api_client.delete(
+        f'/records-security/{sample_records["A"][1].pid.pid_value}')
+    assert resp.status_code == 403
 
+
+    login(api_client, record_deleting_user.id)
     resp = api_client.delete(
         f'/records-security/{sample_records["A"][0].pid.pid_value}')
     assert resp.status_code == 204
